e8d4c75c6dc59084f8a5231d9faaaa53254feb63
Yaroslav Pyrikov <yarko92@rambler.ru>
2024-05-20 14:07:19 +0300

    Правим реализацию resolveArgument

--- src/main/java/com/example/springbootrestpractice/config/UserAuthorizationWebMvcConfig.java
+++ src/main/java/com/example/springbootrestpractice/config/UserAuthorizationWebMvcConfig.java
@@ -6,13 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
-import java.util.Objects;
 
 @Configuration
 public class UserAuthorizationWebMvcConfig implements WebMvcConfigurer {
@@ -31,15 +29,9 @@ public class UserAuthorizationWebMvcConfig implements WebMvcConfigurer {
 
         @Override
         public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-            User user = (User) webRequest.getAttribute("user", RequestAttributes.SCOPE_REQUEST);
-            if (user != null) {
-                if (Objects.equals(parameter.getParameterName(), "name")) {
-                    return user.getName();
-                } else if (Objects.equals(parameter.getParameterName(), "password")) {
-                    return user.getPassword();
-                }
-            }
-            return null;
+            String name = webRequest.getParameter("user");
+            String password = webRequest.getParameter("password");
+            return new User(name, password);
         }
     }
 }
